6c29eead3f510dddcf3dc4d746f8cd88730555f0
Avinash Rajavarapu <rajavarapu.avinash@gmail.com>
2024-11-05 13:15:51 +0530

    Update server.js

--- my-app/server.js
+++ my-app/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const path =  require('path');
 
 // Initialize Express
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './build')))
