fde5dbe969248af501059734d82cca76c1db2849
Avinash Rajavarapu <rajavarapu.avinash@gmail.com>
2024-11-07 12:15:11 +0530

    Update Footer.js

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,9 +1,10 @@
-
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 import whatsapp from './facebook.png';
 import instagram from './instagram.png';
-import twitter from './twitter.png'
+import twitter from './twitter.png';
+
 function Footer() {
   return (
     <footer className="footer">
@@ -11,27 +12,26 @@ function Footer() {
         <div className="footer-col">
           <h3 className="footer-col-title">Company</h3>
           <ul className="footer-col-list">
-            <li><a href="/Aboutus">About Us</a></li>
-            <li><a href="/Contact">Contact Us</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Blog</a></li>
+            <li><Link to="/Aboutus">About Us</Link></li>
+            <li><Link to="/Contact">Contact Us</Link></li>
+            <li><Link to="#">Careers</Link></li>
+            <li><Link to="#">Blog</Link></li>
           </ul>
         </div>
         <div className="footer-col">
           <h3 className="footer-col-title">Resources</h3>
           <ul className="footer-col-list">
-            <li><a href="/Faq">FAQ</a></li>
-            <li><a href="/Privacy">Privacy Policy</a></li>
-            <li><a href="/TermsOfService">Terms of Service</a></li>
+            <li><Link to="/Faq">FAQ</Link></li>
+            <li><Link to="/Privacy">Privacy Policy</Link></li>
+            <li><Link to="/TermsOfService">Terms of Service</Link></li>
           </ul>
         </div>
         <div className="footer-col1">
           <h3 className="footer-col-title1">Social</h3>
           <ul className="footer-col-listf">
-          
-          <a href="https://www.facebook.com/Ransackorg"> <img src={ whatsapp }  className="abn"/></a>
-          <a href="https://twitter.com/Ransackorg?t=1ADz84PwqLixS3_fJVU-OQ&s=09">  <img src={ twitter }  className="abn"/></a>
-          <a href="https://instagram.com/ransack__?igshid=ZDdkNTZiNTM=" >  <img src={instagram} className="abn"/></a>
+            <a href="https://www.facebook.com/Ransackorg"> <img src={whatsapp} className="abn" alt="Facebook"/></a>
+            <a href="https://twitter.com/Ransackorg?t=1ADz84PwqLixS3_fJVU-OQ&s=09">  <img src={twitter} className="abn" alt="Twitter"/></a>
+            <a href="https://instagram.com/ransack__?igshid=ZDdkNTZiNTM=" >  <img src={instagram} className="abn" alt="Instagram"/></a>
           </ul>
         </div>
       </div>
@@ -43,4 +43,5 @@ function Footer() {
 }
 
 export default Footer;
+
 
